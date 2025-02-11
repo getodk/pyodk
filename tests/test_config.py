4b6021604e820e71622449e79b621cb44f141ed2
spwoodcock <sam.woodcock@protonmail.com>
2025-02-11 16:06:26 +0000

    test: new env var config option

--- tests/test_config.py
+++ tests/test_config.py
@@ -92,3 +92,79 @@ class TestConfig(TestCase):
         self.assertEqual(
             "Config value 'password' must not be empty.", err.exception.args[0]
         )
+
+    def test_read_envvars____ok__required_vars_present(self):
+        """Successfully reads environment variables as PyOdk config."""
+        cfg = {
+            "PYODK_CENTRAL_URL": "https://env.vars.config.com",
+            "PYODK_CENTRAL_USER": "user@env.vars.config.com",
+            "PYODK_CENTRAL_PASS": "DifferentPassword",
+        }
+
+        with patch.dict(os.environ, cfg, clear=True):
+            new_config = config.read_config()
+            self.assertEqual(new_config.central.base_url, cfg["PYODK_CENTRAL_URL"])
+            self.assertEqual(new_config.central.username, cfg["PYODK_CENTRAL_USER"])
+            self.assertEqual(new_config.central.password, cfg["PYODK_CENTRAL_PASS"])
+
+    def test_read_envvars__error_missing_required_env_var(self):
+        """Raise an error when no config is provided and required env vars are missing."""
+        cfg = {
+            "PYODK_CENTRAL_USER": "https://env.vars.config.com",
+            "PYODK_CENTRAL_PASS": "user@env.vars.config.com",
+            # Missing PYODK_CENTRAL_URL
+        }
+        with patch.dict(os.environ, cfg, clear=True):
+            with self.assertRaises(PyODKError) as err:
+                config.read_config()
+            self.assertIn("No valid configuration found", str(err.exception))
+
+    def test_read_envvars__ok__with_empty_default_project_id(self):
+        """All required env vars are set, but PYODK_DEFAULT_PROJECT_ID is empty string."""
+        cfg = {
+            "PYODK_CENTRAL_URL": "https://env.vars.config.com",
+            "PYODK_CENTRAL_USER": "user@env.vars.config.com",
+            "PYODK_CENTRAL_PASS": "DifferentPassword",
+            "PYODK_DEFAULT_PROJECT_ID": "",  # Empty string should be ignored
+        }
+        with patch.dict(os.environ, cfg, clear=True):
+            conf = config.read_config()
+            self.assertEqual(conf.central.base_url, cfg["PYODK_CENTRAL_URL"])
+            self.assertEqual(conf.central.username, cfg["PYODK_CENTRAL_USER"])
+            self.assertEqual(conf.central.password, cfg["PYODK_CENTRAL_PASS"])
+            self.assertEqual(conf.central.default_project_id, None)
+
+    def test_read_envvars__ok__with_valid_default_project_id(self):
+        """Return config and include PYODK_DEFAULT_PROJECT_ID when it is provided."""
+        cfg = {
+            "PYODK_CENTRAL_URL": "https://env.vars.config.com",
+            "PYODK_CENTRAL_USER": "user@env.vars.config.com",
+            "PYODK_CENTRAL_PASS": "DifferentPassword",
+            "PYODK_DEFAULT_PROJECT_ID": "123",
+        }
+        with patch.dict(os.environ, cfg, clear=True):
+            conf = config.read_config()
+            self.assertEqual(conf.central.base_url, cfg["PYODK_CENTRAL_URL"])
+            self.assertEqual(conf.central.username, cfg["PYODK_CENTRAL_USER"])
+            self.assertEqual(conf.central.password, cfg["PYODK_CENTRAL_PASS"])
+            self.assertEqual(
+                conf.central.default_project_id, cfg["PYODK_DEFAULT_PROJECT_ID"]
+            )
+
+    def test_read_config__ok__file_takes_precedence(self):
+        """If env vars are present, but a file is specified, it takes precedence."""
+        cfg = {
+            "PYODK_CENTRAL_URL": "https://env.vars.config.com",
+            "PYODK_CENTRAL_USER": "user@env.vars.config.com",
+            "PYODK_CENTRAL_PASS": "DifferentPassword",
+            "PYODK_DEFAULT_PROJECT_ID": "123",
+        }
+        with patch.dict(os.environ, cfg, clear=True):
+            conf = config.read_config(config_path=resources.CONFIG_FILE)
+            # Check the env var values aren't present in config
+            self.assertNotEqual(conf.central.base_url, cfg["PYODK_CENTRAL_URL"])
+            self.assertNotEqual(conf.central.username, cfg["PYODK_CENTRAL_USER"])
+            self.assertNotEqual(conf.central.password, cfg["PYODK_CENTRAL_PASS"])
+            self.assertNotEqual(
+                conf.central.default_project_id, cfg["PYODK_DEFAULT_PROJECT_ID"]
+            )
